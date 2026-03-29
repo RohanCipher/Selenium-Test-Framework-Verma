35a1744bb6ca8f84506371c22b1440838434dc9c
Rohan <babai1993@gmail.com>
2026-03-29 10:43:18 +0530

    modified logger

--- src/main/java/com/orangehrm/utilities/LoggerManager.java
+++ src/main/java/com/orangehrm/utilities/LoggerManager.java
@@ -7,6 +7,6 @@ public class LoggerManager {
 	
 	//This method returns a Logger instance for the provided class
 	public static Logger getLogger(Class<?> clazz ) {
-		return LogManager.getLogger();
+		return LogManager.getLogger(clazz);
 	}
 }
